c0db3093b619409d0506ce9f10aef2ce74957fdf
andreeqm <70642112+andreeqm@users.noreply.github.com>
2022-09-14 18:32:35 +0300

    Add files via upload

---
+++ tema de acasa1.py
@@ -0,0 +1,10 @@
+def creare_lista():
+    n=int(input("nr de elemente:"))
+    lista_locala=[]
+    for i in range(n):
+        elem=int(input("elementul"+str(i)+"este:"))
+        lista_locala.append(elem)
+    return lista_locala
+lista1=creare_lista()
+print(lista1)
+
